45daa0ecabbfc0638c6dacd8d41f48be7cf5031e
David Fryd <david@fryd.com>
2022-08-21 16:46:40 -0400

    feat: App demo v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
+import {fullCompileProcess, RRCompile} from './scripts/compile'
 
 const App = () =>  {
+
+    let code = "";
+    code = '(triangle 200 "solid" "turquoise")'
+    code = '(printf "hello world")'
+    
+    let program = "program name"
+    let debug = true
+
+    let output = ""
+
+    let onDone = (p) => {console.log("done!", p); output = (p)}
+    let onDoneError = (p) => {console.log("ERROR ( but done )", p)}
+
+    let response = RRCompile(program, code, onDone, onDoneError)
+
+    console.log("response: ", response)
+
     return (
-        <h1>Working App</h1> 
+        <>
+            <h1>Working App</h1> 
+            <h3>Code:</h3>
+            <p>{code}</p>
+            <h3>Response Bytecode:</h3>
+            <p>{output}</p>
+        </>
     )
 }
 
